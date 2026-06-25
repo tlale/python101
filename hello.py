c0972f11406d17b8263a148c679fe76ee68636e1
Tlaleng <tlaramoroke@outlook.com>
2026-06-25 14:10:39 -0400

    created first python file!

---
+++ hello.py
@@ -0,0 +1,49 @@
+#testing vsc connection
+print ("hello world")
+'''multiline 
+comment'''
+print("do this if you write more than 1 lone of comment")
+print('My name is Nunu')
+learner = 'tlale'
+print(learner)
+ideal_num_pets = 2
+print(ideal_num_pets)
+how_much_banana = 9.78
+print(how_much_banana)
+# usage
+print("Per Scholas is Unlocking Potential and Changing the Face of Tech")
+print(10+90)
+print(300.5+0.5)
+print(12, 24, -2, sep=':')
+print('but', 'not', 'including', sep='**')
+print('but', 'not', 'including', sep='')
+# checking data types
+is_our_cat_vaccinated = True
+print(is_our_cat_vaccinated)
+#check data type
+print(type(is_our_cat_vaccinated))
+print(type(how_much_banana))
+# use input function
+find area notice the curly brackets with f change the format.output computed output
+length = int(input("Enter the length:"))
+width = int(input("Enter the width:"))
+area = length * width
+print(f"Your total area is {area}") 
+#pay attention to out put of this 2 codes here output string area
+length = int(input("Enter the length:"))
+width = int(input("Enter the width:"))
+area = length * width
+print("Your total area is {area}") 
+
+# create a receipt
+customeName = input("Enter customer's name: ")
+productName =  input("Enter product's name: ")
+unitPrice = float(input("Enter price per item: "))
+quantity = int(input("Enter how many: "))
+totalcost = unitPrice * quantity
+
+print(f"customer: {customeName}")
+print(f"product: {productName}")
+print(f"price: {unitPrice}")
+print(f"quantity:{quantity}")
+print(f"totalCost:{ unitPrice * quantity}") 
